595079c6c015716f02e439a2fb4fb028f9cc3756
dhananjayui07 <dhananjaykumar.list@gmail.com>
2024-03-22 14:05:27 +0530

    sales bot api

--- src/components/wrapper/index.tsx
+++ src/components/wrapper/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { BeakerIcon } from '@heroicons/react/24/solid';
+import React, { useEffect, useState } from 'react';
 import Insights from './insights';
 import ScriptTabContent from './script-tab-content';
 
@@ -15,14 +16,63 @@ const scriptTabs = [
 	{ id: 'tab2', label: 'Transcript', content: <div>Coming Soon.....</div> },
 	{ id: 'tab3', label: 'Feedback', content: <div>Coming Soon..</div> },
 ];
+
 const StaticSales: React.FC = () => {
-	console.log('');
 	const [activeTab, setActiveTab] = useState(scriptTabs[0].id);
+	const [accessToken, setAccessToken] = useState('');
+
+	useEffect(() => {
+		const getAccessToken = async () => {
+			console.log('in access token function');
+			let oauthKey = 'mhrKq8EbncAmw5HfTaqPPK7MHYwHvTAHJakOUtPWthWgYK6k';
+			let oauthSecret = 'KnAEPxNhbwCupculrA0uO5PJlVsmS3r1sy3fwu326SGT6QSbhaAfen32HLGqKe3E';
+
+			const oauthCredentials = btoa(`${oauthKey}:${oauthSecret}`);
+
+			try {
+				console.log('before fetch request start');
+				const response = await fetch(
+					'https://api-dv.brightspeed.com/genai/oauth/client_credential/accesstoken?grant_type=client_credentials',
+					{
+						method: 'POST',
+						headers: {
+							Authorization: `Basic ${oauthCredentials}`,
+							'Content-Type': 'application/json',
+						},
+					}
+				);
+				const data = await response.json();
+				console.log('access_token: ', data.access_token);
+				setAccessToken(data.access_token);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getAccessToken();
+	}, []);
+
 	return (
 		<>
 			<div className="w-80">
 				{/* Script Tabs */}
 				<div className="p-4 pb-5 bg-[#97eeff] rounded-t-xl">
+					<BeakerIcon className="h-6 w-6 text-blue-500" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+						/>
+					</svg>
+
 					<div className="rounded-lg bg-[#7edff2] flex justify-between items-start mb-4">
 						{scriptTabs.map((tab) => (
 							<button
@@ -52,4 +102,5 @@ const StaticSales: React.FC = () => {
 		</>
 	);
 };
+
 export default StaticSales;
--- src/components/wrapper/script-tab-content/index.tsx
+++ src/components/wrapper/script-tab-content/index.tsx
@@ -1,49 +1,122 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+
+interface ApiResponse {
+	instruction?: string;
+	'dialog script': string[];
+}
 
 const ScriptTabContent: React.FC = () => {
-  return (
-    <>
-      <div>
-        <h3 className="mb-2 text-lg leading-6 text-black  font-semibold capitalize">
-          Step 1: Available Plans
-        </h3>
-        <div>
-          {/* response instructions */}
-          <div className="mb-4">
-            <div className="text-black m-0 text-xs leading-4">
-              Select the right plan for the customer’s needs. For customers only
-              qualifying for DSL/Copper, only the best speed available is
-              displayed.
-            </div>
-          </div>
-          {/* Response Dialogues */}
-          <div className="flex flex-col gap-2">
-            <div className="text-black text-xs items-stretch rounded-t-3xl rounded-bl-3xl bg-[#CDF6FF] p-3 border border-[#22C5E6]">
-              <p className="m-0 leading-[18px] font-semibold">
-                “What address are you seeking service for?”
-              </p>
-            </div>
-            <div className="text-black text-xs items-stretch rounded-t-3xl rounded-bl-3xl bg-[#CDF6FF] p-3 border border-[#22C5E6]">
-              <p className="m-0 leading-[18px] font-semibold">
-                “What address are you seeking service for?”
-              </p>
-            </div>
-            <div className="text-black text-xs items-stretch rounded-t-3xl rounded-bl-3xl bg-[#CDF6FF] p-3 border border-[#22C5E6]">
-              <p className="m-0 leading-[18px] font-semibold">
-                “What address are you seeking service for?”
-              </p>
-            </div>
-            <div className="text-black text-xs items-stretch rounded-t-3xl rounded-bl-3xl bg-[#CDF6FF] p-3 border border-[#22C5E6]">
-              <p className="m-0 leading-[18px] font-semibold">
-                “Do you currently reside at this address, or are you moving to
-                this address?”
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	const [accessToken, setAccessToken] = useState('');
+
+	const [dataHistory, setDataHistory] = useState<ApiResponse[]>([]);
+	const [loading, setLoading] = useState<boolean>(false);
+	const [titleText, setTitleText] = useState<string[]>(['Address Search']);
+
+	useEffect(() => {
+		const getAccessToken = async () => {
+			console.log('in access token function');
+			let oauthKey = 'mhrKq8EbncAmw5HfTaqPPK7MHYwHvTAHJakOUtPWthWgYK6k';
+			let oauthSecret = 'KnAEPxNhbwCupculrA0uO5PJlVsmS3r1sy3fwu326SGT6QSbhaAfen32HLGqKe3E';
+
+			const oauthCredentials = btoa(`${oauthKey}:${oauthSecret}`);
+
+			try {
+				console.log('before fetch request start');
+				const response = await fetch(
+					'https://api-dv.brightspeed.com/genai/oauth/client_credential/accesstoken?grant_type=client_credentials',
+					{
+						method: 'POST',
+						headers: {
+							Authorization: `Basic ${oauthCredentials}`,
+							'Content-Type': 'application/json',
+						},
+					}
+				);
+				const data = await response.json();
+				console.log('access_token: ', data.access_token);
+				setAccessToken(data.access_token);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getAccessToken();
+	}, []);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			let data = {
+				step_name: 'Address Search',
+				address_type: '',
+				customer_name: '',
+				date: '',
+				selected_internet_plan: '',
+				selected_phone_plan: '',
+				selected_billing_choice: '',
+				verification_mode: '',
+				service_network_type: '',
+				max_speed: '',
+				last_screen_flag: '',
+			};
+
+			try {
+				console.log('before fetch request start');
+				const response = await fetch('https://api-dv.brightspeed.com/genai-studio/sales_bot/', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${accessToken}`,
+					},
+					body: JSON.stringify(data),
+				});
+
+				const responseData = await response.json();
+				console.log('Sales bot response data:', responseData);
+				setDataHistory([responseData]);
+			} catch (error) {
+				console.log('Error calling API:', error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, [accessToken]);
+	console.log('Sales bot response data history:', dataHistory);
+
+	return (
+		<>
+			<div>
+				<h3 className="mb-2 text-lg leading-6 text-black  font-semibold capitalize">Step 1: Available Plans</h3>
+				<div>
+					{/* response instructions */}
+					<div className="mb-4">
+						<div className="text-black m-0 text-xs leading-4">
+							Select the right plan for the customer’s needs. For customers only qualifying for DSL/Copper, only the
+							best speed available is displayed.
+						</div>
+					</div>
+					{/* Response Dialogues */}
+					<div className="flex flex-col gap-2">
+						<div className="text-black text-xs items-stretch rounded-t-3xl rounded-bl-3xl bg-[#CDF6FF] p-3 border border-[#22C5E6]">
+							<p className="m-0 leading-[18px] font-semibold">“What address are you seeking service for?”</p>
+						</div>
+						<div className="text-black text-xs items-stretch rounded-t-3xl rounded-bl-3xl bg-[#CDF6FF] p-3 border border-[#22C5E6]">
+							<p className="m-0 leading-[18px] font-semibold">“What address are you seeking service for?”</p>
+						</div>
+						<div className="text-black text-xs items-stretch rounded-t-3xl rounded-bl-3xl bg-[#CDF6FF] p-3 border border-[#22C5E6]">
+							<p className="m-0 leading-[18px] font-semibold">“What address are you seeking service for?”</p>
+						</div>
+						<div className="text-black text-xs items-stretch rounded-t-3xl rounded-bl-3xl bg-[#CDF6FF] p-3 border border-[#22C5E6]">
+							<p className="m-0 leading-[18px] font-semibold">
+								“Do you currently reside at this address, or are you moving to this address?”
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default ScriptTabContent;
